e0fc1b35e21fc4552f00ccf6406893e2594a16b0
Romy Andre <romyandre79@gmail.com>
2026-03-12 14:51:44 +0700

    update user weight

--- components/PricingCalculator.vue
+++ components/PricingCalculator.vue
@@ -423,9 +423,9 @@ const LICENSE_MULTIPLIER = 12
 const SOURCE_CODE_MULTIPLIER = 30
 
 const USER_WEIGHTS = {
-  admin: 2,
-  operational: 1,
-  viewer: 0.5
+  admin: 1,
+  operational: 0.5,
+  viewer: 0.25
 }
 
 // PAYG Rates
